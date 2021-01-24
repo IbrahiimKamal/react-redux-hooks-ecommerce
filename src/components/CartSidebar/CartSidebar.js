632e208d53d6ddba4222612dbf08b501157c3a57
Ibrahim <geeksacademy225@gmail.com>
2021-01-24 10:12:34 +0200

    add cart empty image

--- src/components/CartSidebar/CartSidebar.js
+++ src/components/CartSidebar/CartSidebar.js
@@ -8,6 +8,8 @@ import { formatPrice } from '../../utils/formatPrice';
 import { closeSideCart } from '../../redux/sidebar/sidebar_actions';
 import { removeFromCart } from '../../redux/products/products_actions';
 
+import cartEmptyImg from '../../assets/images/cart-empty.jpg';
+
 import './CartSidebar.scss';
 
 const CartSidebar = () => {
@@ -45,37 +47,43 @@ const CartSidebar = () => {
           </span>
         </div>
         <div className="cart-sidebar__content">
-          {cart.length > 0
-            ? cart.map((item) => {
-                return (
-                  <div key={item.id} className="cart-sidebar__products">
-                    <div className="cart-sidebar__product-image-container">
-                      <img
-                        className="cart-sidebar__product-image"
-                        src={item.image}
-                        alt="product"
-                      />
-                    </div>
-                    <div className="cart-sidebar__product-info">
-                      <p className="cart-sidebar__product-name">{item.name}</p>
-                      <div className="cart-sidebar__prices">
-                        <p className="cart-sidebar__product-qty">
-                          {item.qty} X
-                        </p>
-                        <p className="cart-sidebar__product-price">
-                          {formatPrice(item.price)}
-                        </p>
-                        <p className="cart-sidebar__delete">
-                          <MdDelete
-                            onClick={() => dispatch(removeFromCart(item.id))}
-                          />
-                        </p>
-                      </div>
+          {cart.length > 0 ? (
+            cart.map((item) => {
+              return (
+                <div key={item.id} className="cart-sidebar__products">
+                  <div className="cart-sidebar__product-image-container">
+                    <img
+                      className="cart-sidebar__product-image"
+                      src={item.image}
+                      alt="product"
+                    />
+                  </div>
+                  <div className="cart-sidebar__product-info">
+                    <p className="cart-sidebar__product-name">{item.name}</p>
+                    <div className="cart-sidebar__prices">
+                      <p className="cart-sidebar__product-qty">{item.qty} X</p>
+                      <p className="cart-sidebar__product-price">
+                        {formatPrice(item.price)}
+                      </p>
+                      <p className="cart-sidebar__delete">
+                        <MdDelete
+                          onClick={() => dispatch(removeFromCart(item.id))}
+                        />
+                      </p>
                     </div>
                   </div>
-                );
-              })
-            : null}
+                </div>
+              );
+            })
+          ) : (
+            <div className="cart-sidebar__empty-image-container">
+              <img
+                className="cart-sidebar__empty-image"
+                src={cartEmptyImg}
+                alt="cart is empty"
+              />
+            </div>
+          )}
         </div>
         <div className="cart-sidebar__footer">
           Total: {formatPrice(totalPrice)}{' '}
